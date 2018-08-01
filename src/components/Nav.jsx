6421a4525654309cfd2a126aa6847689687395b9
Julian Muratori <julianmuratori@gmail.com>
2018-08-01 15:59:05 -0400

    working on the menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,18 +3,50 @@ import './css/Nav.css'
 
 class Nav extends Component {
     state = {
-        isOpen: false
+        isActive: false,
+        menuOptions: ['LOGIN', 'SEARCH', 'DISCOVER']
+    }
+
+    handleClick = () => {
+        const { isActive } = this.state
+        this.setState({ isActive: !isActive })
+    }
+
+    menuMap = (i) => {
+        const { menuOptions } = this.state
+        return (
+            <div className="menu__link">
+                <a href="#">
+                    <h3>{i}</h3>
+                </a>
+            </div>
+        )
     }
 
     
     render() {
+        const { isActive, menuOptions } = this.state
+        
         return (
             <div className="nav">
-               <div className="menuBar">
+                <div className="menuBar" onClick={this.handleClick}>
                     <div className="menuBar__left">
-                        <div className="menuBar__left--burger"></div>
+                        <div className="menuBar__left--burger">
+                        </div>
                     </div>
+                    <div className="menuBar__center">
+                        <h3>MENU</h3>
+                    </div>
+                    <div className="menuBar__right"></div>
                </div>
+               
+                <div className='menu'>
+                    <ul className='menu__container'>
+                        {
+                            isActive ? (menuOptions.map(this.menuMap)) : (null)
+                        }
+                    </ul>
+                </div>                                   
             </div>
         )
     }
